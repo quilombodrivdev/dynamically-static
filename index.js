f760a9a6255ab97efd938950586115190f7ded17
Alan Campora <alan.campora@sovanta.com>
2017-03-06 16:33:04 -0300

    [ADD] setup express

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express'), 
+    bodyParser = require('body-parser')
+    app = express(), 
+    config = {
+        ip: "127.0.0.1", 
+        port: "3500" 
+    };
+
+app.use(bodyParser.json());
+
+app.listen(config.port,config.ip, function(){
+    console.log('server listening on:' , config.port, config.ip);
+});
+
+
+app.post('/post/comment', function(req,res){
+    console.log(req.body);
+    res.json({data:'success'});
+});
+
+
