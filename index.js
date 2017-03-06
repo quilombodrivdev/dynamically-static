9797d7961340828123ac72eb58339389272daf75
Alan Campora <alan.campora@gmail.com>
2017-03-06 17:41:39 -0300

    [ADD] hash

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express'),
     bodyParser = require('body-parser')
+fs = require('fs')
 app = express(),
     config = {
         server: {
             ip: "127.0.0.1",
             port: "3500"
-        }, 
-        site:{
-            dist: "_data/comments" 
+        },
+        site: {
+            dist: "/_data/comments/"
         }
     };
 
@@ -15,13 +16,21 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.listen(config.port, config.ip, function() {
-    console.log('server listening on:', config.port, config.ip);
+app.listen(config.server.port, config.server.ip, function() {
+    console.log('server listening on:', config.server.port, config.server.ip);
 });
 
 app.post('/post/comment', function(req, res) {
     var entry = req.body;
-    console.log(req.body);
+    var hash = new Date().getTime().toString();
+    var distFile = config.site.dist + entry.slug + "/" + hash + ".yml";
+    //fs.writeFile("", "Hey there!", function(err) {
+        //if (err) {
+            //return console.log(err);
+        //}
+
+        //console.log("The file was saved!");
+    //});
     res.json({
         data: "success"
     });
