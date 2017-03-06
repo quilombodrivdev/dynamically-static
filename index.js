2539dc2bc2ba428e69065fff0bd4a0ad9b510e34
Alan Campora <alan.campora@gmail.com>
2017-03-06 17:32:17 -0300

    [ADD] post data comming successfully

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express'), 
+var express = require('express'),
     bodyParser = require('body-parser')
-    app = express(), 
+app = express(),
     config = {
-        ip: "127.0.0.1", 
-        port: "3500" 
+        server: {
+            ip: "127.0.0.1",
+            port: "3500"
+        }, 
+        site:{
+            dist: "_data/comments" 
+        }
     };
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.listen(config.port,config.ip, function(){
-    console.log('server listening on:' , config.port, config.ip);
+app.listen(config.port, config.ip, function() {
+    console.log('server listening on:', config.port, config.ip);
 });
 
-app.post('/post/comment', function(req,res){
+app.post('/post/comment', function(req, res) {
+    var entry = req.body;
     console.log(req.body);
-    res.json({data:'success'});
+    res.json({
+        data: "success"
+    });
 });
-
-
