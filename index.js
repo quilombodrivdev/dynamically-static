ceedeb9fa9f427aad9ac9cc43f7490f9872c0f1d
alancampora <alan.campora@gmail.com>
2017-03-07 01:48:53 -0300

    [ADD] git integration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     fs = require('fs'),
     mkdirp = require('mkdirp'),
+    shjs = require('shelljs/global'),
     app = express(),
     config = {
         server: {
@@ -10,6 +11,14 @@ var express = require('express'),
         },
         site: {
             dist: "_data/comments/"
+        },
+        git: {
+            username: "alancampora",
+            email: "alan.campora@gmail.com",
+            repo: "quilombodrivdev/quilombodrivdev.github.io.git",
+            branch: "comments",
+            remote: "blog",
+            commitMessage: "New comment - medusa integration"
         }
     };
 
@@ -32,10 +41,19 @@ app.post('/post/comment', function(req, res) {
     }
 
     this._fileCreation(distFolder, distFile, data)
-        .then(function(){
-            console.log("file successfully created"); 
+        .then(function() {
+            this._git(distFile);
+        }.bind(this))
+        .then(function() {
+            res.json({
+                "success": "success"
+            });
 
-            res.json({"success":"success"});
+        })
+        .catch(function(err) {
+            res.json({
+                "error": err
+            });
         });
 
 });
@@ -43,7 +61,7 @@ app.post('/post/comment', function(req, res) {
 _fileCreation = function(distFolder, distFile, data) {
     return new Promise(function(resolve, reject) {
         mkdirp(distFolder, function(err) {
-            if (err) reject("file creation"); 
+            if (err) reject("file creation");
 
             fs.writeFile(distFile, data, function(err) {
                 if (err) reject("file writing");
@@ -52,5 +70,42 @@ _fileCreation = function(distFolder, distFile, data) {
             });
 
         });
+    })
+};
+
+_git = function(file) {
+    this._getToken()
+        .then(function(token) {
+            console.log('this is the tokeeen:', token);
+            return this._submitPullRequest(file, token);
+        });
+
+};
+
+_getToken = function() {
+    return new Promise(function(resolve, reject) {
+        fs.readFile(".gittoken", 'utf8', function(err, data) {
+            if (err) reject(err);
+            resolve(data.replace(/(?:\r\n|\r|\n)/g, ''));
+        })
+    });
+}
+_submitPullRequest = function(file, token) {
+    return new Promise(function(resolve, reject) {
+
+        console.log('this is a file', file);
+        var gitOrigin = "https://" + config.git.username + ":" + token + "@github.com/" + config.git.repo;
+
+        exec('git config user.name ' + config.git.username);
+        exec('git config user.email ' + config.git.email);
+        exec('git remote add ' + config.git.remote + ' ' + gitOrigin);
+        exec('git pull ' + config.git.remote + ' ' + config.git.branch);
+        exec('git checkout ' + config.git.branch);
+        exec('git add ' + file);
+        exec('git commit -m  "' + config.git.commitMessage + '"');
+        exec('git push --quiet  ' + config.git.remote + ' ' + config.git.branch);
+
+        //console.log('pull request');
+        res('pull request success');
     })
 }
